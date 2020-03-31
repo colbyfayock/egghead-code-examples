35e4d6d7f7678deeb3adae66ff56f6d09bef7759
Colby Fayock <fayock@gmail.com>
2020-03-30 23:35:44 -0400

    creating a new map using react leaflet

---
+++ create-new-map-react-leaflet/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'leaflet/dist/leaflet.css';
+
+import { Map, TileLayer } from 'react-leaflet';
+
+function App() {
+  return (
+    <div className="App">
+      <Map center={[0, 0]} zoom={4}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="&copy; <a href=&quot;https://www.openstreetmap.org/copyright&quot;>OpenStreetMap</a> contributors" />
+      </Map>
+    </div>
+  );
+}
+
+export default App;
