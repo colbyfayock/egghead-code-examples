2a8fcae822d827f41306740c29e514a71970d3fd
Colby Fayock <fayock@gmail.com>
2020-04-05 22:45:40 -0400

    formatting

--- add-marker-popup-to-map-react-leaflet/src/App.js
+++ add-marker-popup-to-map-react-leaflet/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import L from 'leaflet';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
 import './App.css';
+import 'leaflet/dist/leaflet.css';
 
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
-    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-    iconUrl: require('leaflet/dist/images/marker-icon.png'),
-    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
 });
 
 const location = [38.9072, -77.0369];
@@ -21,7 +21,7 @@ function App() {
       <Map center={location} zoom={zoom}>
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="&copy; <a href=&quot;https://www.openstreetmap.org/copyright&quot;>OpenStreetMap</a> contributors" />
         <Marker position={location}>
-          <Popup>Hello, Washington, DC!</Popup>
+          <Popup>Hello, Washington, DC</Popup>
         </Marker>
       </Map>
     </div>
