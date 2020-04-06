e0f885703b842056e50a9c9a1e2a982644b6b462
Colby Fayock <fayock@gmail.com>
2020-04-06 18:10:58 -0400

    importing images from locally stored assets to avoid codesandbox bug

--- add-marker-popup-to-map-react-leaflet/src/App.js
+++ add-marker-popup-to-map-react-leaflet/src/App.js
@@ -6,11 +6,23 @@ import 'leaflet/dist/leaflet.css';
 
 delete L.Icon.Default.prototype._getIconUrl;
 
+// Importing images from locally stored assets to address a bug
+// in CodeSandbox: https://github.com/codesandbox/codesandbox-client/issues/3845
+
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+  iconRetinaUrl: require('./images/marker-icon-2x.png'),
+  iconUrl: require('./images/marker-icon.png'),
+  shadowUrl: require('./images/marker-shadow.png')
 });
+
+// When importing into your own app outside of CodeSandbox, you can import directly
+// from the leaflet package like below
+//
+// L.Icon.Default.mergeOptions({
+//   iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+//   iconUrl: require('leaflet/dist/images/marker-icon.png'),
+//   shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+// });
 
 const location = [38.9072, -77.0369];
 const zoom = 12;
