5ac195320b8d69403a26265420dc09f01813a510
Colby Fayock <fayock@gmail.com>
2020-04-05 21:55:41 -0400

    example for adding a marker and popup to a map with react leaflet

---
+++ add-marker-popup-to-map-react-leaflet/README.md
@@ -0,0 +1,17 @@
+# Create a new map using React Leaflet
+
+React Leaflet map example using the `Map` and `TileLayer` components with a OpenStreetMap basemap.
+
+## Getting Started
+* Install dependencies
+```
+yarn install
+or
+npm install
+```
+* Start a local development server
+```
+yarn start
+or
+npm run start
+```
---
+++ add-marker-popup-to-map-react-leaflet/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import L from 'leaflet';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import './App.css';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+
+const location = [38.9072, -77.0369];
+const zoom = 12;
+
+function App() {
+  return (
+    <div className="App">
+      <Map center={location} zoom={zoom}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="&copy; <a href=&quot;https://www.openstreetmap.org/copyright&quot;>OpenStreetMap</a> contributors" />
+        <Marker position={location}>
+          <Popup>Hello, Washington, DC!</Popup>
+        </Marker>
+      </Map>
+    </div>
+  );
+}
+
+export default App;
