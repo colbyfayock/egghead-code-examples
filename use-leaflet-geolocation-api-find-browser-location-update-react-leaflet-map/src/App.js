aaeb5897e1c05f0d32366f6df7a8195e1fa5945a
Colby Fayock <fayock@gmail.com>
2020-04-26 22:02:30 -0400

    adding lesson to find a browser's location and update the map view with a circle

---
+++ use-leaflet-geolocation-api-find-browser-location-update-react-leaflet-map/src/App.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useRef } from 'react';
+import L from 'leaflet';
+import { Map, TileLayer } from 'react-leaflet';
+import './App.css';
+import 'leaflet/dist/leaflet.css';
+
+const defaultCenter = [0, 0];
+const defaultZoom = 4;
+
+function App() {
+  const mapRef = useRef();
+
+  useEffect(() => {
+    const { current = {} } = mapRef;
+    const { leafletElement: map } = current;
+
+    map.locate({
+      setView: true,
+      maxZoom: 13
+    });
+
+    map.on('locationfound', handleOnLocationFound);
+    map.on('locationerror', handleOnLocationError);
+
+    return () => {
+      map.off('locationfound', handleOnLocationFound);
+      map.off('locationerror', handleOnLocationError);
+    }
+  }, []);
+
+  /**
+   * handleOnLocationFound
+   * @param {object} event Leaflet LocationEvent object
+   */
+
+  function handleOnLocationFound(event) {
+    const { current = {} } = mapRef;
+    const { leafletElement: map } = current;
+
+    const latlng = event.latlng;
+    const radius = event.accuracy;
+    const circle = L.circle(latlng, radius);
+
+    circle.addTo(map);
+  }
+
+  /**
+   * handleOnLocationError
+   * @param {object} error Leaflet ErrorEvent object
+   */
+
+  function handleOnLocationError(error) {
+    alert(`Unable to determine location: ${error.message}`);
+  }
+
+  return (
+    <div className="App">
+      <Map ref={mapRef} center={defaultCenter} zoom={defaultZoom}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="&copy; <a href=&quot;https://www.openstreetmap.org/copyright&quot;>OpenStreetMap</a> contributors" />
+      </Map>
+    </div>
+  );
+}
+
+export default App;
