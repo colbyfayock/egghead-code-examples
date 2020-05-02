19781ca33a230e36699a21ef16cae60102a16d8e
Colby Fayock <fayock@gmail.com>
2020-05-01 20:55:36 -0400

    adding event handler to fullscreen demo

--- add-native-fullscreen-mode-react-leaflet-map-leafletfullscreen/src/App.js
+++ add-native-fullscreen-mode-react-leaflet-map-leafletfullscreen/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Map, TileLayer } from 'react-leaflet';
 import 'leaflet-fullscreen/dist/Leaflet.fullscreen.js';
 import './App.css';
@@ -9,9 +9,23 @@ const defaultCenter = [0, 0];
 const defaultZoom = 4;
 
 function App() {
+  const mapRef = useRef();
+
+  useEffect(() => {
+    const { current = {} } = mapRef;
+    const { leafletElement: map } = current;
+    map.on('fullscreenchange', handleOnToggleFullscreen);
+  }, []);
+
+  function handleOnToggleFullscreen() {
+    const { current = {} } = mapRef;
+    const { leafletElement: map } = current;
+    console.log(`Fullscreen: ${map.isFullscreen() ? 'yes' : 'no'}`);
+  }
+
   return (
     <div className="App">
-      <Map fullscreenControl={true} center={defaultCenter} zoom={defaultZoom}>
+      <Map ref={mapRef} fullscreenControl={true} center={defaultCenter} zoom={defaultZoom}>
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="&copy; <a href=&quot;https://www.openstreetmap.org/copyright&quot;>OpenStreetMap</a> contributors" />
       </Map>
     </div>
