cad03218e2337717dcf6cdcedd22c5416e092118
colbyfayock <fayock@gmail.com>
2020-06-30 23:14:10 -0400

    adding new lesson examples

---
+++ collection-source-and-create-nodes-with-data-from-an-api-in-gatsby/01-request-data-from-custom-api-endpoint-source-nodes-in-gatsby-with-gatsby-node/gatsby-node.js
@@ -0,0 +1,17 @@
+const fetch = require('node-fetch');
+
+exports.sourceNodes = async () => {
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+  const json = await response.json();
+  const { results = [] } = json;
+
+  const pokemon = await Promise.all(results.map(async result => {
+    const { url } = result;
+    const pokeResponse = await fetch(url);
+    return await pokeResponse.json();
+  }));
+
+  console.log('pokemon', pokemon);
+
+  return;
+};
